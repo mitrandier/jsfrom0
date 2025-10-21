e371fa1d55dfec28b1ad23130f1ed37708ebfa16
Mehdi <mitrandier@users.noreply.github.com>
2025-10-21 11:31:15 +0330

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//alert("hallo wellcomen sie!")
+console.log("hallo wieder");
+console.warn("beachten sie!");
+console.error("das ist ein error");
+// 
+document.write("this is text from doc");
+document.getElementById("content").innerHTML='this is update'
