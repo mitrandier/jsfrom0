6dc204db2f7d7ebeb051450adba88b225e2ca752
Mehdi <mitrandier@users.noreply.github.com>
2025-10-21 12:40:25 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,4 +4,59 @@ console.warn("beachten sie!");
 console.error("das ist ein error");
 // 
 document.write("this is text from doc");
+
 document.getElementById("content").innerHTML='this is update'
+
+
+
+
+let fname = "mehdi";
+fname = "miti";
+console.log(fname);
+
+const version = 0.1;
+
+console.log(version)
+
+const age = 40;
+console.log(typeof age);
+
+const firstName = 'ali';
+console.log(typeof firstName)
+
+const isDone = false;
+console.log(typeof isDone);
+
+let x = null;
+console.log(typeof x);
+
+let c = undefined;
+console.log(typeof c);
+
+
+x = age * 5 ;
+console.log ( x );
+console.log(typeof age);
+// -- ++ 
+x++
+console.log(x)
+ x+=10
+ console.log(x)
+
+
+ let name = "miti";
+ let Lname = "kaveh";
+ let fullName = " hello " + "    " + name + "    " + Lname
+
+ fullName = `hallo ${ name } wie gehts? ${Lname}`
+console.log(fullName);
+console.log(fullName.length);
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
+console.log(fullName.substring(0,5).toUpperCase())
+
+
+
+
+
+
